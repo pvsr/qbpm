67ac1d03f6b5bc45c4e0f50d3ed07208ec015c9c
Peter Rice <peter@peterrice.xyz>
2021-12-06 23:00:19 -0500

    add wofi to supported menus

--- qbpm/utils.py
+++ qbpm/utils.py
@@ -8,7 +8,7 @@ from typing import Optional
 
 from xdg import BaseDirectory  # type: ignore
 
-SUPPORTED_MENUS = ["rofi", "dmenu", "dmenu-wl", "applescript"]
+SUPPORTED_MENUS = ["wofi", "rofi", "dmenu", "dmenu-wl", "applescript"]
 
 
 def error(msg: str) -> None:
@@ -37,7 +37,9 @@ def user_config_dir() -> Path:
 def get_default_menu() -> Optional[str]:
     if sys.platform == "darwin":
         return "applescript"
-    for menu_cmd in ["rofi", "dmenu-wl", "dmenu"]:
+    for menu_cmd in SUPPORTED_MENUS:
+        if menu_cmd == "applescript":
+            continue
         if which(menu_cmd) is not None:
             return menu_cmd
     return None
