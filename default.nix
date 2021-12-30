60ae8dd44f300810fd5b50f0240872193af4ce37
Peter Rice <peter@peterrice.xyz>
2021-12-29 23:35:56 -0500

    write a man page
    closes #25

--- default.nix
+++ default.nix
@@ -10,11 +10,14 @@ buildPythonPackage rec {
   src = ./.;
   doCheck = true;
   SETUPTOOLS_SCM_PRETEND_VERSION = version;
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [ pkgs.scdoc setuptools-scm ];
   propagatedBuildInputs = [ pyxdg ];
   checkInputs = [ pytest ];
   postInstall = ''
     mkdir -p $out/share/fish/vendor_completions.d
     cp completions/qbpm.fish $out/share/fish/vendor_completions.d/
+
+    mkdir -p $out/share/man/man1
+    scdoc < qbpm.1.scd > $out/share/man/man1/qbpm.1
   '';
 }
