d00e7a1763f45a7768b45fef8f8d9393e000a1f6
Peter Rice <peter@peterrice.xyz>
2022-02-17 21:34:29 -0500

    complete all commands

--- completions/qbpm.fish
+++ completions/qbpm.fish
@@ -9,10 +9,10 @@ function __fish_qbpm
   eval qbpm $global_args $saved_args
 end
 
-set -l commands new from-session desktop launch run list edit
+set -l commands new from-session desktop launch run list edit choose
 
 complete -c qbpm -f
-complete -c qbpm -n "not __fish_seen_subcommand_from $commands" -a "launch new from-session edit list"
+complete -c qbpm -n "not __fish_seen_subcommand_from $commands" -a "$commands"
 complete -c qbpm -n "__fish_seen_subcommand_from launch edit desktop" -a "(__fish_qbpm list)"
 set -l data_home (set -q XDG_DATA_HOME; and echo $XDG_DATA_HOME; or echo ~/.local/share)
 complete -c qbpm -n "__fish_seen_subcommand_from from-session" -a "(ls $data_home/qutebrowser/sessions | xargs basename -a -s .yml)"
