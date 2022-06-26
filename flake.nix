76381a456ed13acdec338153fbbe0e2e62a092f2
Peter Rice <peter@peterrice.xyz>
2022-06-26 16:21:19 -0400

    use new default flake output pattern

--- flake.nix
+++ flake.nix
@@ -3,17 +3,24 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        packages = flake-utils.lib.flattenTree {
-          qbpm = import ./. { inherit pkgs; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree rec {
+          qbpm = import ./. {inherit pkgs;};
+          default = qbpm;
         };
-        defaultPackage = packages.qbpm;
-        apps.qbpm = flake-utils.lib.mkApp { drv = packages.qbpm; };
-        defaultApp = apps.qbpm;
-        devShell = import ./shell.nix { inherit pkgs; };
+        apps = rec {
+          qbpm = flake-utils.lib.mkApp {drv = packages.qbpm;};
+          default = qbpm;
+        };
+        devShell = import ./shell.nix {inherit pkgs;};
       }
     );
 }
