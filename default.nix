31680569e1587c9a3accb909605b62b3c9bd54d3
Peter Rice <peter@peterrice.xyz>
2023-03-05 15:00:42 -0500

    format default.nix

--- default.nix
+++ default.nix
@@ -1,23 +1,23 @@
-{ pkgs ? import <nixpkgs> { }
-, python ? "python3"
-, pythonPackages ? builtins.getAttr (python + "Packages") pkgs
+{
+  pkgs ? import <nixpkgs> {},
+  python ? "python3",
+  pythonPackages ? builtins.getAttr (python + "Packages") pkgs,
 }:
-
 with pythonPackages;
-buildPythonPackage rec {
-  pname = "qbpm";
-  version = "0.6";
-  src = ./.;
-  doCheck = true;
-  SETUPTOOLS_SCM_PRETEND_VERSION = version;
-  nativeBuildInputs = [ pkgs.scdoc setuptools-scm ];
-  propagatedBuildInputs = [ pyxdg ];
-  checkInputs = [ pytest ];
-  postInstall = ''
-    mkdir -p $out/share/fish/vendor_completions.d
-    cp completions/qbpm.fish $out/share/fish/vendor_completions.d/
+  buildPythonPackage rec {
+    pname = "qbpm";
+    version = "0.6";
+    src = ./.;
+    doCheck = true;
+    SETUPTOOLS_SCM_PRETEND_VERSION = version;
+    nativeBuildInputs = [pkgs.scdoc setuptools-scm];
+    propagatedBuildInputs = [pyxdg];
+    checkInputs = [pytest];
+    postInstall = ''
+      mkdir -p $out/share/fish/vendor_completions.d
+      cp completions/qbpm.fish $out/share/fish/vendor_completions.d/
 
-    mkdir -p $out/share/man/man1
-    scdoc < qbpm.1.scd > $out/share/man/man1/qbpm.1
-  '';
-}
+      mkdir -p $out/share/man/man1
+      scdoc < qbpm.1.scd > $out/share/man/man1/qbpm.1
+    '';
+  }
