0dfd053ebc90164202e1f8ec5d1ec1b8d85e6a27
Peter Rice <peter@peterrice.xyz>
2023-03-05 20:50:48 -0500

    cli tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,25 @@
 from os import environ
 from pathlib import Path
 
+from click.testing import CliRunner
+
 from qbpm.main import main
 
 
 def test_profile_dir_option(tmp_path: Path):
-    main(["-P", str(tmp_path), "new", "test"])
+    runner = CliRunner()
+    result = runner.invoke(main, ["-P", str(tmp_path), "new", "test"])
+    assert result.exit_code == 0
+    assert result.output.strip() == str(tmp_path / "test")
     assert list(tmp_path.iterdir()) == [tmp_path / "test"]
 
 
 def test_profile_dir_env(tmp_path: Path):
     environ["QBPM_PROFILE_DIR"] = str(tmp_path)
-    main(["new", "test"])
+    runner = CliRunner()
+    result = runner.invoke(main, ["new", "test"])
+    assert result.exit_code == 0
+    assert result.output.strip() == str(tmp_path / "test")
     assert list(tmp_path.iterdir()) == [tmp_path / "test"]
 
 
@@ -19,5 +27,8 @@ def test_from_session(tmp_path: Path):
     environ["QBPM_PROFILE_DIR"] = str(tmp_path)
     session = tmp_path / "test.yml"
     session.touch()
-    main(["from-session", str(session)])
+    runner = CliRunner()
+    result = runner.invoke(main, ["from-session", str(session)])
+    assert result.exit_code == 0
+    assert result.output.strip() == str(tmp_path / "test")
     assert set(tmp_path.iterdir()) == {session, tmp_path / "test"}
