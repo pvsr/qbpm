47a80cd2c7dd7a4cefb2265fd232f521e421312f
Peter Rice <peter@peterrice.xyz>
2024-04-23 23:52:33 -0400

    install generated bash and zsh completions

--- default.nix
+++ default.nix
@@ -14,8 +14,11 @@ with pythonPackages;
     propagatedBuildInputs = [pyxdg click];
     checkInputs = [pytest];
     postInstall = ''
-      mkdir -p $out/share/fish/vendor_completions.d
-      cp completions/qbpm.fish $out/share/fish/vendor_completions.d/
+      install -D -m644 completions/qbpm.fish $out/share/fish/vendor_completions.d/qbpm.fish
+
+      install -vd $out/share/{bash-completion/completions,zsh/site-functions}
+      _QBPM_COMPLETE=bash_source $out/bin/qbpm > $out/share/bash-completion/completions/qbpm
+      _QBPM_COMPLETE=zsh_source $out/bin/qbpm > $out/share/zsh/site-functions/qbpm
 
       mkdir -p $out/share/man/man1
       scdoc < qbpm.1.scd > $out/share/man/man1/qbpm.1
