738a64002b59cf6a786879f29b09c163b894ad84
Peter Rice <peterrice@fastmail.com>
2024-08-21 23:58:16 -0400

    format nix files with nixfmt-rfc-style

--- default.nix
+++ default.nix
@@ -1,28 +1,35 @@
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs ? import <nixpkgs> { },
   python ? "python3",
   pythonPackages ? builtins.getAttr (python + "Packages") pkgs,
 }:
-with pythonPackages; let
+with pythonPackages;
+let
   pyproject = pkgs.lib.importTOML ./pyproject.toml;
 in
-  buildPythonPackage rec {
-    pname = pyproject.project.name;
-    inherit (pyproject.project) version;
-    src = ./.;
-    doCheck = true;
-    format = "pyproject";
-    nativeBuildInputs = [pkgs.scdoc setuptools];
-    propagatedBuildInputs = [pyxdg click];
-    checkInputs = [pytest];
-    postInstall = ''
-      install -D -m644 completions/qbpm.fish $out/share/fish/vendor_completions.d/qbpm.fish
+buildPythonPackage rec {
+  pname = pyproject.project.name;
+  inherit (pyproject.project) version;
+  src = ./.;
+  doCheck = true;
+  format = "pyproject";
+  nativeBuildInputs = [
+    pkgs.scdoc
+    setuptools
+  ];
+  propagatedBuildInputs = [
+    pyxdg
+    click
+  ];
+  checkInputs = [ pytest ];
+  postInstall = ''
+    install -D -m644 completions/qbpm.fish $out/share/fish/vendor_completions.d/qbpm.fish
 
-      install -vd $out/share/{bash-completion/completions,zsh/site-functions}
-      _QBPM_COMPLETE=bash_source $out/bin/qbpm > $out/share/bash-completion/completions/qbpm
-      _QBPM_COMPLETE=zsh_source $out/bin/qbpm > $out/share/zsh/site-functions/qbpm
+    install -vd $out/share/{bash-completion/completions,zsh/site-functions}
+    _QBPM_COMPLETE=bash_source $out/bin/qbpm > $out/share/bash-completion/completions/qbpm
+    _QBPM_COMPLETE=zsh_source $out/bin/qbpm > $out/share/zsh/site-functions/qbpm
 
-      mkdir -p $out/share/man/man1
-      scdoc < qbpm.1.scd > $out/share/man/man1/qbpm.1
-    '';
-  }
+    mkdir -p $out/share/man/man1
+    scdoc < qbpm.1.scd > $out/share/man/man1/qbpm.1
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -6,45 +6,53 @@
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
   inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    treefmt-nix,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
         treefmt = treefmt-nix.lib.evalModule pkgs {
           projectRootFile = "flake.nix";
           programs.mypy.enable = true;
           programs.mypy.directories."." = {
-            modules = ["src/qbpm" "tests"];
+            modules = [
+              "src/qbpm"
+              "tests"
+            ];
             extraPythonPackages = self.packages.x86_64-linux.default.propagatedBuildInputs;
           };
           programs.ruff.check = true;
           programs.ruff.format = true;
-          programs.alejandra.enable = true;
+          programs.nixfmt-rfc-style.enable = true;
         };
-        package = import ./. {inherit pkgs;};
-        app = flake-utils.lib.mkApp {drv = package;};
-      in {
+        package = import ./. { inherit pkgs; };
+        app = flake-utils.lib.mkApp { drv = package; };
+      in
+      {
         packages.qbpm = package;
         packages.default = package;
         apps.qbpm = app;
         apps.default = app;
 
         devShells.default = pkgs.mkShell {
-          packages = with pkgs;
+          packages =
+            with pkgs;
             [
               ruff
-              (python3.withPackages (ps:
-                with ps; [
+              (python3.withPackages (
+                ps: with ps; [
                   pytest
                   mypy
                   pylsp-mypy
                   ruff-lsp
-                ]))
+                ]
+              ))
             ]
             ++ self.packages.${system}.default.propagatedBuildInputs;
         };
